8e61762a3d8877a19edf122ca3d4b9a9f30ffcb0
Igor Guimarães <igorgui.tec@gmail.com>
2022-11-14 17:32:27 -0300

    -

--- src/app.js
+++ src/app.js
@@ -12,36 +12,44 @@ let db;
 mongoClient.connect().then(() => {
     db = mongoClient.db("participants")
 })
+
 const app = express()
 app.use(express.json())
 app.use(cors())
 
 const nameSchema = joi.object({ name: joi.string().required() })
+const messageSchema = joi.object({to: joi.string().required(), text: joi.string().required()})
 
 app.post('/participants', async (req, res) => {
-    const { name } = req.body
-    const validation = nameSchema.validate(name, { abortEarly: true })
+    const name = req.body
+    let validation = nameSchema.validate(name, { abortEarly: true })
 
     if (validation.error) {
         const erros = validation.error.details.map((detail) => detail.message);
         res.status(422).send(erros);
         return;
     }
-    const alreadyNamed = db.colection("participants").findOne(name)
+    // const alreadyNamed = db.collection("participants").findOne(name)
 
-    if(alreadyNamed){
-        res.sendStatus(409)
-        return;
-    }
+    // if (alreadyNamed) {
+    //     res.sendStatus(409)
+    //     return;
+    // }
 
 
     try {
-        await db.colection("participants").insertOne({ name: name, lastStatus: Date.now() })
+        await db.collection("participants").insertOne({ name: name.name, lastStatus: Date.now() })
         res.sendStatus(201)
     } catch (err) {
         console.log(err)
         res.sendStatus(500)
     }
+})
+
+app.post('/messages', async (req, res) => {
+    const message = req.body;
+    const validation = messageSchema = joi.object(message, {abortEarly: true})
+
 })
 
 app.get((req, res) => {
